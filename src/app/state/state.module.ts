24c5625036bee8bf097324726ee5998bd4c3ec31
sauvageb <sauvageboris.pro@gmail.com>
2022-06-17 15:45:31 +0200

    using reducers in state module

--- src/app/state/state.module.ts
+++ src/app/state/state.module.ts
@@ -3,13 +3,14 @@ import {CommonModule} from '@angular/common';
 import {StoreModule} from "@ngrx/store";
 import {environment} from "../../environments/environment";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {reducers} from "./index";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot([]),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode

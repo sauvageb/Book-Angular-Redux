756d276989cf41fc207b2d72a1f5597ec79e337f
sauvageb <sauvageboris.pro@gmail.com>
2022-06-17 15:57:05 +0200

    remove and pay methods in book-selection

--- src/app/components/book-selection/book-selection.component.ts
+++ src/app/components/book-selection/book-selection.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {Book} from "../../models/book/book.model";
+import {Store} from "@ngrx/store";
+import {AppState} from "../../state";
+import {ACTION_BOOK_SELECTION_PAY, ACTION_BOOK_SELECTION_REMOVE} from "../../state/book-selection.action";
 
 @Component({
   selector: 'app-book-selection',
@@ -11,10 +14,16 @@ export class BookSelectionComponent {
   @Input()
   books: Book[] = [];
 
+  constructor(private store: Store<AppState>) {
+  }
+
   removeFromCollection(book: Book) {
+    this.store.dispatch(ACTION_BOOK_SELECTION_REMOVE({bookToRemove: book}));
   }
 
   paySelection() {
+    this.store.dispatch(ACTION_BOOK_SELECTION_PAY({books: this.books}));
   }
+
 
 }

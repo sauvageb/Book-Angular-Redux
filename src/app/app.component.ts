5d3cb58a197b072aa31946ef1d5bfb713db5b5e9
sauvageb <sauvageboris.pro@gmail.com>
2022-06-17 17:01:33 +0200

    using action (and effect with reducer) im app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {Book} from "./models/book/book.model";
 import {BookService} from "./services/book.service";
 import {Store} from "@ngrx/store";
-import {AppState} from "./state";
+import {AppState, booksSelector} from "./state";
+import {ACTION_LOAD_BOOKS} from "./state/book-collection/book-collection.action";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  books$: Observable<Book[]> = this.bookService.getBooks();
-  booksSelection$: Observable<Book[]> = this.store.select('selectedBooks');
-
-  // booksSelection$: Observable<Book[]> = this.bookService.getBooks();
+  books$!: Observable<Book[]>;
+  booksSelection$!: Observable<Book[]>;
 
   constructor(private store: Store<AppState>,
               private bookService: BookService) {
+  }
+
+  ngOnInit(): void {
+    this.booksSelection$ = this.store.select('selectedBooks');
+
+    this.books$ = this.store.select(booksSelector);
+    this.store.dispatch(ACTION_LOAD_BOOKS());
   }
 
 }

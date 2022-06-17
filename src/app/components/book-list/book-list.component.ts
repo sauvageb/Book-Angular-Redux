798eeeffd88552b675c22f5ff5a90acdcc14b4c1
sauvageb <sauvageboris.pro@gmail.com>
2022-06-17 15:53:13 +0200

    method add book to collection

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,24 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Book} from "../../models/book/book.model";
-import {BookService} from "../../services/book.service";
+import {AppState} from "../../state";
+import {Store} from "@ngrx/store";
+import {ACTION_BOOK_SELECTION_ADD} from "../../state/book-selection.action";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent {
 
   @Input()
   books!: Book[];
 
-  constructor(private bookService: BookService) {
-  }
-
-  ngOnInit(): void {
+  constructor(
+    private store: Store<AppState>) {
   }
 
   addToCollection(book: Book) {
+    this.store.dispatch(ACTION_BOOK_SELECTION_ADD({book: book}));
   }
 
 }
